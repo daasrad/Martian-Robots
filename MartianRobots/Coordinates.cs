69532bf6ff5dc65b922711d5c3d0ba1f5c6ea7c9
daasrad <daasrad001@gmail.com>
2022-03-09 19:33:44 +0000

    Refactored Grid class

---
+++ MartianRobots/Coordinates.cs
@@ -0,0 +1,14 @@
+﻿namespace MartianRobots
+{
+    class Coordinates
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Coordinates(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+    }
+}
--- MartianRobots/Grid.cs
+++ MartianRobots/Grid.cs
@@ -4,18 +4,19 @@ namespace MartianRobots
 {
     public class Grid
     {
-        private int XSize { get; set; }
-        private int YSize { get; set; }
+        private const int MinimumGridSize = 0;
+        private const int MaximumGridSize = 50;
+
+        private readonly Coordinates GridSize;
 
         public Grid(int xSize, int ySize)
         {
-            if(xSize > 50 || ySize > 50 || xSize < 0 || ySize < 0)
+            if(xSize < MinimumGridSize || ySize < MinimumGridSize || xSize > MaximumGridSize || ySize > MaximumGridSize)
             {
                 throw new ArgumentException("Grid Size must be between 0 and 50");
             }
 
-            this.XSize = xSize;
-            this.YSize = ySize;
+            GridSize = new Coordinates(xSize, ySize);
         }
     }
 }
